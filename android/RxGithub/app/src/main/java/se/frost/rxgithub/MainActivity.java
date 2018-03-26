dae7cc1d49ae139691da002e35e11699e7d3d3d9
Sergii Nezdolii <sergii@frost.se>
2018-03-26 18:37:56 +0200

    [UI] updating main activity UI to show view with JSON

--- android/RxGithub/app/src/main/java/se/frost/rxgithub/MainActivity.java
+++ android/RxGithub/app/src/main/java/se/frost/rxgithub/MainActivity.java
@@ -3,11 +3,30 @@ package se.frost.rxgithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pddstudio.highlightjs.HighlightJsView;
+import com.pddstudio.highlightjs.models.Language;
+import com.pddstudio.highlightjs.models.Theme;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @BindView(R.id.jsonView)
+    HighlightJsView jsonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initJsonView();
+    }
+
+    private void initJsonView() {
+        jsonView.setTheme(Theme.SOLARIZED_DARK);
+        jsonView.setHighlightLanguage(Language.JSON);
+        jsonView.setSource("{\"hello\":\"world\"}");
+        jsonView.reload();
+    }
 }
