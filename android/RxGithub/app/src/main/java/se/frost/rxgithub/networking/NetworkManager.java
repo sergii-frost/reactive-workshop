4474787c1a60152206c8ee9296b435a323abf6c0
Sergii Nezdolii <sergii@frost.se>
2018-03-26 20:26:59 +0200

    [DEV] more setup of api client

--- android/RxGithub/app/src/main/java/se/frost/rxgithub/networking/NetworkManager.java
+++ android/RxGithub/app/src/main/java/se/frost/rxgithub/networking/NetworkManager.java
@@ -4,6 +4,8 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import se.frost.rxgithub.BuildConfig;
 
 /**
@@ -41,6 +43,8 @@ public class NetworkManager {
     private GithubApiClient buildApiClient() {
         return new GithubApiClient(new Retrofit.Builder()
                 .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(buildOkHttpClient())
                 .build()
                 .create(GithubApi.class)
