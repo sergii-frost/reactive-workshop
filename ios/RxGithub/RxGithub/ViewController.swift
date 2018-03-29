2cd272598272fc8411b94fd446f392fd638ed1e0
Sergii Nezdolii <sergii@frost.se>
2018-03-29 09:42:44 +0200

    [ADD] initial iOS project setup

---
+++ ios/RxGithub/RxGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxGithub
+//
+//  Created by Sergii Frost on 2018-03-29.
+//  Copyright © 2018 Frost°. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
